cfe9f152f163da609f35fc346fb6badcc0a7fc88
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-22 21:47:11 +0600

    outline

--- manager.cpp
+++ manager.cpp
@@ -1,15 +1,28 @@
-
-
-// TODO todo
-// Manager
 // Password
+//  Fields
+//      label, text
+//  Methods
+//      getters/setters
 
 
+// Manager
+//  Fields
+//      file
+//      filename
+//      user
+//      Password: master_password
+//      passwords vector
+
+//  Methods
+//      constructor takes username&password and opens file and calls readFromFile(master_password)
+//      read from file - read file and decipher and fill passwords vector
+//      create_password / delete_password / alter_password
+//      user menu
 
 // class User
 // Fields
 //    Username
-//    Password
+//    Password: master_password
 //    Password_hint
 
 //    Settings
