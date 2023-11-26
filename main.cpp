e1964677952a3b26d9820d4b6ee038ff2998fb25
RomanchenkoAS <RomanchenkoAS@yandex.ru>
2023-11-26 15:38:19 +0600

    user auth test

--- main.cpp
+++ main.cpp
@@ -5,10 +5,19 @@ using namespace std;
 
 int main() {
 
-    cout << "hello passworder" << endl;
+    string username = "artur";
 
-    PasswordHasher hash( "hello");
-    cout << hash.getHash() << endl;
+//    string name_hash = PasswordHasher(username).getHash();
+//    cout << name_hash << endl;
+//    cout << "Master password instance:\n";
+//    MasterPassword mp(PasswordHasher(username).getHash());
+
+    User user(username);
+    string password = "1234";
+    user.auth_sequence(password);
+    if (user.isAuthorized()) {
+        Manager(&user, user.getFilename());
+    }
     return 0;
 
 
